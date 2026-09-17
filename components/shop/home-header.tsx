2be7beb668ae86f4ba636752dd5cabcb6957e3e4
emergoapp-beep <emergoapp@gmail.com>
2026-09-17 20:39:07 +0200

    Fix: import mancante di next/image in home-header

--- components/shop/home-header.tsx
+++ components/shop/home-header.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import Link from 'next/link'
+import Image from 'next/image'
 import { useState, useEffect, useRef } from 'react'
 import { Menu, X, ShoppingBag, User } from 'lucide-react'
 import { toast } from 'sonner'
